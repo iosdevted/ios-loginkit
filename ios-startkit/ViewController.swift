47f6ee3e95e2de9c106e1e22d11ccae90574f657
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2020-09-30 15:09:25 +0900

    initial commit

---
+++ ios-startkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-startkit
+//
+//  Created by Ted Hyeong on 30/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
